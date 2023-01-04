6a46b42e7de89ce3cd1873a00078c0defd4aff63
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2023-01-03 22:01:19 -0600

    cambiando la ruta del fetch de localhost al url del server

--- clientSide/script.js
+++ clientSide/script.js
@@ -10,10 +10,10 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
+       
         element.textContent += '.';
 
-        // If the loading indicator has reached three dots, reset it
+       
         if (element.textContent === '....') {
             element.textContent = '';
         }
@@ -33,9 +33,7 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
+//genera id unico para cada caracter, asi se genera el efecto de escritura
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -67,26 +65,26 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form)
 
-    // user's chatstripe
+    // user
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // to clear the textarea input 
+    
     form.reset()
 
-    // bot's chatstripe
+    // bot
     const uniqueId = generateUniqueId()
     chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
+    
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
+    
     const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
+    
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://gpt3codex.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -101,7 +99,7 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        const parsedData = data.bot.trim() 
 
         typeText(messageDiv, parsedData)
     } else {
